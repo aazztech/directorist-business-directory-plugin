558b2653bc821c4e54d26e9e20787ceaa81257b4
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2025-02-03 12:25:41 +0600

    Fix: Remember Me Field

--- includes/model/Account.php
+++ includes/model/Account.php
@@ -155,6 +155,8 @@ class Directorist_Account {
 			'author_role_label'         => $atts['author_role_label'],
 			'user_role_label'           => $atts['user_role_label'],
 			'active_form'               => $active_form,
+			'display_rememberme'        => get_directorist_option( 'display_rememberme' ),
+			'log_rememberme'			=> get_directorist_option( 'log_rememberme' ),
 		];
 
 		return Helper::get_template_contents( 'account/login-registration-form', $args );
