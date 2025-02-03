44467a881677e6a88fd865ca6d4f8d41f07f09c2
obiPlabon <obiplabon@gmail.com>
2025-02-03 12:14:06 +0600

    Added php rector

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+	->withPaths([
+		// __DIR__ . '/config.php',
+		// __DIR__ . '/directorist-base.php',
+		__DIR__ . '/includes',
+	])
+	->withRootFiles()
+    // register single rule
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class
+    ])
+    // here we can define, what prepared sets of rules will be applied
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+		typeDeclarations: true
+    )
+	->withPhpSets(php70: true);
